d1d5ce5e81d50c587a7b1b43b9c38ad5bfaad37a
dhoffman548 <112724937+dylhoff53@users.noreply.github.com>
2022-11-18 11:53:13 -0700

    DevRepo

---
+++ SOLID Project/Assets/_Project/Scripts/SelectionManager.cs
@@ -0,0 +1,49 @@
+﻿﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+
+    private ISelectionResponse _selectionResponse;
+
+    private Transform _selection;
+
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+
+        _selectionResponse = GetComponent<ISelectionResponse>(); 
+    }
+
+    private void Update()
+    {
+        // Deselection/Selection Response
+        if (_selection != null)
+        {
+            _selectionResponse.OnDeselect(_selection);
+        }
+
+        //Creating a Ray
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Selection Determination
+        _selection = null;
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                _selection = selection;
+            }
+        }
+
+        //Deslection/Selection Response
+        if (_selection != null)
+        {
+            _selectionResponse.OnSelect(_selection);
+        }
+    }
+}
